c80c94ad088a76383b9911af6b3cdcb719923b9b
jdelfino <jdelfino@gmail.com>
2014-07-05 18:18:14 -0400

    getting ridiculous now

--- codechef/dishown.cpp
+++ codechef/dishown.cpp
@@ -9,22 +9,32 @@
 #include <stdlib.h>
 
 using namespace std;
-typedef tuple<int,int,int> Test;
+//typedef tuple<int,int,int> Test;
+
+struct Dish {
+	Dish *next;
+	int num;
+	Dish() : next(NULL) {}
+	Dish(int i) : next(NULL), num(i) {}
+};
 
 struct Chef {
 	int id;
-	vector<int> dishes;
+	Dish head;
+	Dish *tail;
 	int score;
-
-	inline Chef(int id_, const vector<int> &dishes_, int score_) :
-	id(id_), dishes(dishes_), score(score_) 
-	{ 
-		dishes.reserve(4);
-	}
-
-	Chef(): Chef(-1, vector<int>(), -1) {}
 };
 
+struct Test {
+	int x;
+	int y;
+	int t;
+
+	Test(int x_) : x(x_), t(1) { }
+	Test(int x_, int y_) : x(x_), y(y_), t(0) {}
+};
+
+/*
 std::ostream& operator<<(std::ostream &strm, const Chef &chef) {
 	strm << "Chef(id: " << chef.id << ", score: " << chef.score << ", dishes: [";
 	for(auto dish : chef.dishes) {
@@ -32,14 +42,14 @@ std::ostream& operator<<(std::ostream &strm, const Chef &chef) {
 	}
 	strm << "])";
 	return strm;
-}
+}*/
 
 inline void output(const std::string &val){
 	fwrite(val.c_str(), sizeof(char), val.size(), stdout);
 }
 
-vector<Chef> *chefs;
-vector<Chef*> *dish_to_chef;
+Chef chefs[10000];
+Chef* dish_to_chef[10000];
 
 float total_init = 0.0;
 float total_comp = 0.0;
@@ -51,13 +61,12 @@ void do_test(const vector<int> &scores, const vector<Test> &queries){
 	//stringstream answers;
 
 	for(int i = 0; i < scores.size(); ++i){
-		Chef &c = (*chefs)[i];
+		Chef &c = chefs[i];
 		c.id = i;
-		c.dishes.clear();
-		c.dishes.push_back(i);
+		c.head = Dish(i);
+		c.tail = &c.head;
 		c.score = scores[i];
-		(*dish_to_chef)[i] = &c;
-
+		dish_to_chef[i] = &c;
 	}
 
 	//total_init += (clock() - io_start) / (double) CLOCKS_PER_SEC;
@@ -66,10 +75,10 @@ void do_test(const vector<int> &scores, const vector<Test> &queries){
 	for(const auto &query : queries){
 		//cout << "(" << get<0>(query) << ", " << get<1>(query) << ", " << get<2>(query) << ")" << endl;
 
-		if(get<0>(query) == 0){
+		if(query.t == 0){
 			//clock_t lstart = clock();
-			Chef *c1 = (*dish_to_chef)[get<1>(query)];
-			Chef *c2 = (*dish_to_chef)[get<2>(query)];
+			Chef *c1 = dish_to_chef[query.x];
+			Chef *c2 = dish_to_chef[query.y];
 
 			if(c1 == c2) {
 				fwrite("Invalid query!\n", sizeof(char), 15, stdout);
@@ -84,15 +93,23 @@ void do_test(const vector<int> &scores, const vector<Test> &queries){
 					//cout << "winner: " << winner->id << " loser: " << loser->id << endl;
 					//clock_t ins_start = clock();
 
-					for(int od : loser->dishes){
-						(*dish_to_chef)[od] = winner;
+					Dish *d = &loser->head;
+					while(d) {
+						dish_to_chef[d->num] = winner;
+						d = d->next;
 					}
+					winner->tail->next = &loser->head;
+					winner->tail = loser->tail;
+
+					//for(int od : loser->dishes){
+					//	dish_to_chef[od] = winner;
+					//}
 
 					//winner->dishes.reserve(winner->dishes.size() + loser->dishes.size());
-					winner->dishes.insert(
-						winner->dishes.end(), 
-						loser->dishes.begin(), 
-						loser->dishes.end());
+					//winner->dishes.insert(
+					//	winner->dishes.end(), 
+					//	loser->dishes.begin(), 
+					//	loser->dishes.end());
 					//total_insert += (clock() - ins_start) / (double) CLOCKS_PER_SEC;
 					//loser->dishes = vector<int>();
 				}
@@ -102,7 +119,7 @@ void do_test(const vector<int> &scores, const vector<Test> &queries){
 		} else {
 			//clock_t ins_start = clock();
 
-			int answ = (*dish_to_chef)[get<1>(query)]->id + 1;
+			int answ = dish_to_chef[query.x]->id + 1;
 			//answers << answ << "\n";
 			/*
 			char cansw[6];
@@ -131,7 +148,7 @@ void do_test(const vector<int> &scores, const vector<Test> &queries){
 				p /= 10;
 			}
 			putc_unlocked('\n', stdout);
-			
+
 			/*
  			char cansw[7];
 			sprintf(cansw, "%d\n", answ);
@@ -178,8 +195,8 @@ int main(int argc, char** argv){
 
 	int T = read_int();
 
-	chefs = new vector<Chef>(10000);
-	dish_to_chef = new vector<Chef*>(10000);
+	//chefs = new Chef[10000];
+	//dish_to_chef = new Chef*[10000];
 
 	for(int test = 0; test < T; ++test){
 		int N = read_int();
@@ -201,9 +218,9 @@ int main(int argc, char** argv){
 			int tp = read_int();
 
 			if(tp == 0){
-				queries->push_back(make_tuple(0, read_int()-1, read_int()-1));
+				queries->push_back(Test(read_int()-1, read_int()-1));
 			} else {
-				queries->push_back(make_tuple(1, read_int()-1, -1));
+				queries->push_back(Test(read_int()-1));
 			}
 		}
 		
@@ -211,8 +228,6 @@ int main(int argc, char** argv){
 
 		//clock_t do_start = clock();
 		do_test(scores, *queries);
-		//total_do += (clock() - do_start) / (double) CLOCKS_PER_SEC;
-
 	}
 	//cerr << "Grand total " << setprecision(10) << ( clock() - start ) / (double) CLOCKS_PER_SEC << " IO " << total_io << endl;
 	//cerr << "init " << total_init << " do " << total_do << " comp " << total_comp << " ins " << total_insert << " lookup " << total_lookup << endl;
